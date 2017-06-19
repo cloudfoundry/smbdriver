f28604c694828ea892ace069b462062ff55b3781
Abel Hu <abelch@microsoft.com>
2017-06-19 15:35:51 +0800

    Implement smbdriver

---
+++ README.md
@@ -0,0 +1,2 @@
+# smbdriver
+This driver mounts SMB shares. For more information about using this driver in your Cloud Foundry, visit the [smb-volume-release git repository](https://github.com/AbelHu/smb-volume-release).
---
+++ driveradmin/resources.go
@@ -0,0 +1,33 @@
+package driveradmin
+
+import (
+	"code.cloudfoundry.org/voldriver"
+	"github.com/tedsuo/rata"
+)
+
+const (
+	EvacuateRoute = "evacuate"
+	PingRoute = "ping"
+)
+
+var Routes = rata.Routes{
+	{Path: "/evacuate", Method: "GET", Name: EvacuateRoute},
+	{Path: "/ping", Method: "GET", Name: PingRoute},
+}
+
+//go:generate counterfeiter -o smbdriverfakes/fake_driver_admin.go src/github.com/AbelHu/smbdriver/driveradmin DriverAdmin
+
+type DriverAdmin interface {
+	Evacuate(env voldriver.Env) ErrorResponse
+	Ping(env voldriver.Env) ErrorResponse
+}
+
+type ErrorResponse struct {
+	Err string
+}
+
+//go:generate counterfeiter -o smbdriverfakes/fake_drainable.go src/github.com/AbelHu/smbdriver/driveradmin Drainable
+type Drainable interface {
+	Drain(env voldriver.Env) error
+}
+
